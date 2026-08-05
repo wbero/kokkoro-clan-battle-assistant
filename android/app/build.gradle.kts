dc3006af995c8020106c17e51708825263570942
wbero <1533445310@qq.com>
2026-08-05 18:49:45 +0800

    release: bump version to 1.2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.kokkoro.clanbattle"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12001
-        versionName = "1.2.1"
+        versionCode = 12002
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
